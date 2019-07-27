9080dbd2b54857f53c7c3e0399f6dbfe88ccbd2b
Jinyu Wu <q835771840@gmail.com>
2019-07-27 14:42:04 -0500

    finished article list

--- src/page/home/store/reducer.js
+++ src/page/home/store/reducer.js
@@ -10,9 +10,27 @@ const defaultState = fromJS({
     },
     {
       id: 2,
-      title: "随便热点",
+      title: "随便",
       imgUrl:
         "https://n.sinaimg.cn/sinacn13/448/w1024h1024/20180504/7b5f-fzyqqiq8228305.jpg"
+    }
+  ],
+  articleList: [
+    {
+      id: 1,
+      title: "树欲静而风不止，子欲养而亲不待!",
+      desc:
+        "对门的孟阿姨夜里突然走了，走的悄无声息，所有人都不知道，留下了自己五岁多的儿子和十岁多女儿，孟阿姨怎么突然走了，所有人都感到很意外。因为就在孟阿...",
+      imgUrl:
+        "http://www.chinesenzherald.co.nz/assets/Uploads/News/blog-24138/_resampled/FillWyI2MjAiLCIzMTAiXQ/1121958435-15107092545121n.jpeg"
+    },
+    {
+      id: 2,
+      title: "树欲静而风不止，子欲养而亲不待!",
+      desc:
+        "对门的孟阿姨夜里突然走了，走的悄无声息，所有人都不知道，留下了自己五岁多的儿子和十岁多女儿，孟阿姨怎么突然走了，所有人都感到很意外。因为就在孟阿...",
+      imgUrl:
+        "http://www.chinesenzherald.co.nz/assets/Uploads/News/blog-24138/_resampled/FillWyI2MjAiLCIzMTAiXQ/1121958435-15107092545121n.jpeg"
     }
   ]
 });
